1a0f049c665a07227c2122efcec2c68a9f9361c5
Brian Zwahr <echosa@echosa.net>
2019-10-15 18:08:59 -0500

    Refactor

--- main.c
+++ main.c
@@ -21,35 +21,66 @@ struct PackageManager {
 /*********
 * Functions
 *********/
-struct PackageManager definePackageManager(char name[10], char targetPackage[]) {
+struct PackageManagerCommand definePackageManagerCommand(char name[10], char commandArgs[50], bool requiresRoot) {
+    struct PackageManagerCommand command;
+    sprintf(command.args, "%s %s", name, commandArgs);
+    command.requiresRoot = requiresRoot;
+
+    return command;
+}
+
+struct PackageManager definePackageManager(char name[10], struct PackageManagerCommand searchCommand) {
     struct PackageManager manager;
     strcpy( manager.name, name);
-
-    struct PackageManagerCommand searchCommand;
-    char command[50];
-    sprintf(command, "%s %s %s", manager.name, "search", targetPackage);
-    strcpy(searchCommand.args, command);
     manager.searchCommand = searchCommand;
 
     return manager;
 }
 
+void runCommand(struct PackageManager manager, char *command) {
+    if (strcmp(command, "search") == 0) {
+        system(manager.searchCommand.args);
+    } else {
+        printf("Unrecognized command: %s\n", command);
+    }
+}
+
+
+/*********
+* Package Managers
+*********/
+struct PackageManager defineAptPackageManager(char targetPackage[]) {
+    // Name
+    char name[10] = "apt";
+
+    // Search Command
+    char searchCommandArgs[50];
+    sprintf(searchCommandArgs, "search %s", targetPackage);
+    struct PackageManagerCommand searchCommand = definePackageManagerCommand(name, searchCommandArgs, false);
+
+    return definePackageManager(name, searchCommand);;
+}
+
+/*********
+* Main
+*********/
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Please specify a package name.\n");
+    if (argc < 3) {
+        printf("Usage: %s <command> <package>\n", argv[0]);
         return 1;
     }
 
-    char *targetPackage = argv[1];
+    char *command = argv[1];
+    char *targetPackage = argv[2];
     struct PackageManager managers[] = {
-         definePackageManager("apt", targetPackage)
+         defineAptPackageManager(targetPackage)
     };
 
     for (int i = 0; i < SIZEOF(managers); i++) {
         printf("####################\n");
         printf("####################\n");
         printf("%s\n", managers[i].name);
-        system(managers[i].searchCommand.args);
+        runCommand(managers[i], command);
         printf("####################\n" );
     }
 
